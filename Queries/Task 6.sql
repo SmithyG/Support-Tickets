0daa1b1daea8c2f2bfcf5921744f1707e2950181
George Smith <smithy_g@live.com>
2018-05-02 15:45:47 +0100

    Change joins in Task 6 to FULL OUTER

--- Queries/Task 6.sql
+++ Queries/Task 6.sql
@@ -4,7 +4,7 @@ SELECT
   tU.updatetime,
   s.name
 FROM ticket t
-  INNER JOIN ticketupdate tU ON t.ticketid = tU.ticketid
-  INNER JOIN staff s ON tU.staffid = s.staffid
+  FULL OUTER JOIN ticketupdate tU ON t.ticketid = tU.ticketid
+  FULL OUTER JOIN staff s ON tU.staffid = s.staffid
 WHERE (t.ticketid = 101)
 ORDER BY t.ticketid, tU.updatetime;
